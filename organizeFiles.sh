7f19736a9bec07608743f2c5283b241af9c1c023
Mainuddin Alam Irteja <mainuddinalamirteja@gmail.com>
2024-08-04 07:53:57 -0300

    Introducing the script

--- organizeFiles.sh
+++ organizeFiles.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-# Bash script to organize files in a directory based on their extension
+# Bash script to organize files in a directory based on their extensions
+
+# Introduce the script to the user
+echo "Hello there. This bash script helps organizing files based on their extensions.\n It will create folders for each file extension"
